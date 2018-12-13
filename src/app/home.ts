a6bcb27c4cf7e4d8c045245697ebdfa5173c33df
judydoctrine@gmail.com <sy@nwg-yamasaki03.local>
2018-12-13 19:18:58 +0900

    home css addition

--- src/app/home.ts
+++ src/app/home.ts
@@ -2,7 +2,10 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'ons-page[home]',
-  template: require('./home.html')
+  template: require('./home.html'),
+  styles: [
+  	'./home.css'
+  ]
 })
 export class Home {
 
