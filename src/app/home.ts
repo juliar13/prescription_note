b7dabefb713c38170e74e3ac366ac2c75d9eb7a7
judydoctrine@gmail.com <sy@nwg-yamasaki03.local>
2018-12-13 18:56:58 +0900

    home.html addition

--- src/app/home.ts
+++ src/app/home.ts
@@ -2,16 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'ons-page[home]',
-  template: `
-    <div class="content">
-      <p>I am the first tab.</p>
-      <p> 20181213 1010 yamasaki added</p>
-      <ul>
-        <li> list 1</li>
-        <li> list 2</li>
-      </ul>
-    </div>
-  `
+  template: require('./home.html')
 })
 export class Home {
   constructor() {}
