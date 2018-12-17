b8078cd61d733dce884e2d8baa627c2fed3052e5
juliar13 <judydoctrine@gmail.com>
2018-12-17 13:48:31 +0900

    pattern 0 ngFor

--- src/app/home.ts
+++ src/app/home.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'ons-page[home]',
-  template: require('./home.html'),
+  templateUrl: 'src/app/home.html',
   styles: [
   	'./home.css'
   ]
@@ -14,6 +14,33 @@ export class Home {
 
   title: string='No.1 Date: 2018/12/13';
   memo: string='桜病院での処方';
+
+  lists1: {img1: string, date_str: string, memo_str: string, img2: string}[]=[
+    {
+      img1: '/www/assets/img/user.png',
+      date_str: 'No.1 Date: 2018/12/13',
+      memo_str: 'メモテスト',
+      img2: '/www/assets/img/test.jpeg',
+    },
+    {
+      img1: '/www/assets/img/user.png',
+      date_str: 'No.2 Date: 2018/12/13',
+      memo_str: 'I am yamasaki',
+      img2: '/www/assets/img/test.jpeg',
+    },
+    {
+      img1: '/www/assets/img/user.png',
+      date_str: 'No.3 Date: 2018/12/13',
+      memo_str: '桜病院fsfpqafでの処方',
+      img2: '/www/assets/img/test.jpeg',
+    },
+    {
+      img1: '/www/assets/img/user.png',
+      date_str: 'No.4 Date: 2018/12/13',
+      memo_str: '桜病院fs;afj;jfafでの処方',
+      img2: '/www/assets/img/test.jpeg',
+    },
+  ];
 
   // アラートは多分使わないからそのうち消す
   onItemClick(): Void {
