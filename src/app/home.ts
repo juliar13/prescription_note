51615e0b851b468a58f13af8a7ca25ca3e061242
juliar13 <judydoctrine@gmail.com>
2018-12-17 13:54:53 +0900

    pattern 1 ngFor

--- src/app/home.ts
+++ src/app/home.ts
@@ -42,6 +42,33 @@ export class Home {
     },
   ];
 
+  lists2: {img1: string, date_str: string, memo_str: string, img2: string}[]=[
+    {
+      img1: '/www/assets/img/user.png',
+      date_str: 'No.1 Date: 2018/12/13',
+      memo_str: 'メモテスト',
+      img2: '/www/assets/img/test.jpeg',
+    },
+    {
+      img1: '/www/assets/img/user.png',
+      date_str: 'No.2 Date: 2018/12/13',
+      memo_str: 'I am yamasaki',
+      img2: '/www/assets/img/test.jpeg',
+    },
+    {
+      img1: '/www/assets/img/user.png',
+      date_str: 'No.3 Date: 2018/12/13',
+      memo_str: '桜病院fsfpqafでの処方',
+      img2: '/www/assets/img/test.jpeg',
+    },
+    {
+      img1: '/www/assets/img/user.png',
+      date_str: 'No.4 Date: 2018/12/13',
+      memo_str: '桜病院fs;afj;jfafでの処方',
+      img2: '/www/assets/img/test.jpeg',
+    },
+  ];
+
   // アラートは多分使わないからそのうち消す
   onItemClick(): Void {
     alert('test');
