257c557909c0ff3986a3dc712746f48568a957d8
iam4570 <iam4570@yahoo.co.jp>
2018-12-25 23:41:33 +0900

    データベースにアクセスできるようにした

--- src/app/home.ts
+++ src/app/home.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {PrescriptionRecordRepository} from '../service/prescription-record.repository';
 
 @Component({
   selector: 'ons-page[home]',
@@ -15,6 +16,38 @@ export class Home {
   title: string='No.1 Date: 2018/12/13';
   memo: string='桜病院での処方';
 
-  constructor() {}
+  constructor(
+    private _prescriptionRecordRepository: PrescriptionRecordRepository,
+  ) {
+    Promise.resolve()
+      .then(() => 
+        this._prescriptionRecordRepository.addRecord({
+            id: null,
+            createdDate: new Date(2017, 12, 12).toISOString(),
+            updatedDate: new Date(2017, 12, 13).toISOString(),
+            imagePath: '/tmp/path1',
+            note: 'test note',
+          }))
+      .then(() => 
+        this._prescriptionRecordRepository.addRecord({
+            id: null,
+            createdDate: new Date(2017, 12, 14).toISOString(),
+            updatedDate: new Date(2017, 12, 14).toISOString(),
+            imagePath: '/tmp/path2',
+            note: 'test note',
+          }))
+      .then(() => 
+        this._prescriptionRecordRepository.addRecord({
+            id: null,
+            createdDate: new Date(2017, 12, 15).toISOString(),
+            updatedDate: new Date(2017, 12, 15).toISOString(),
+            imagePath: '/tmp/path3',
+            note: 'test note',
+          }))
+      .then((result) => this._prescriptionRecordRepository.getRecords())
+      .then(result => console.log('record', result))
+      .catch(error => console.log('error', error))
+
+  }
 
 }
