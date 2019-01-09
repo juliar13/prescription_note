6c8b100943c9746d16510fa166151e8f380bb896
juliar13 <judydoctrine@gmail.com>
2019-01-09 20:19:35 +0900

    significantly UI changed

--- src/app/home/edit.ts
+++ src/app/home/edit.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core'
-import {OnsNavigator, Params} from 'ngx-onsenui'
+import {OnsenModule, OnsNavigator, Params} from 'ngx-onsenui'
+import {ListData} from './list_data'
+import {Datas} from './list_data_sample'
+import * as ons from 'onsenui';
 
 @Component({
   selector: 'ons-page[preview]',
@@ -11,6 +14,8 @@ import {OnsNavigator, Params} from 'ngx-onsenui'
 export class Edit {
 
     num: number;
+
+    get_list: ListData[] = Datas;
 
     constructor(
       private _navigator: OnsNavigator,
@@ -25,8 +30,30 @@ export class Edit {
       this._navigator.element.popPage();
     }
 
+    // 画像をタッチした時に呼び出される
+    onImgChange() {
+      console.log("onImgChange");
+      // 写真撮影かアルバムからか選択する画面を出す
+      this.shootOrAlbum();
+    }
+
+    shootOrAlbum() {
+      console.log("shootOrAlbum");
+      ons.notification.alert('撮影かアルバム画面になる');
+    }
+
+    // 登録完了ボタンタッチで呼び出される
     onEditEndClicked() {
-      console.log("Edit End Button Clicked");
+      console.log("Registration Button Clicked");
+      let toast_timeout = 2000; // 2000msec
+      var registration_success = true;
+      if (registration_success = true) {
+        ons.notification.toast('Registration Success !!', {timeout: toast_timeout});
+      } else {
+        // 今はこちらは表示されないようにしてある
+        ons.notification.toast('Registration Failed ...', {timeout: toast_timeout});
+      }
+
     }
 
 }
